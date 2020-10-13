3a45642d843c295fc98254adc8633b1a9cd9ff8f
pentamassiv <pentamassiv@posteo.de>
2020-10-13 23:38:59 +0200

    Added update to cursor

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-//! It provides an easy to use interface to use the zwp_input_method_v2 protocol
+//! This crate provides an easy to use interface to use the zwp_input_method_v2 protocol
 //!
+use std::convert::TryInto;
 use std::num::Wrapping;
 use std::sync::{Arc, Mutex};
 use wayland_client::protocol::wl_seat::WlSeat;
@@ -7,6 +8,8 @@ use wayland_client::{Filter, Main};
 use wayland_protocols::unstable::text_input::v3::client::zwp_text_input_v3::{
     ChangeCause, ContentHint, ContentPurpose,
 };
+#[macro_use]
+extern crate log;
 use zwp_input_method::input_method_unstable_v2::zwp_input_method_manager_v2::ZwpInputMethodManagerV2;
 use zwp_input_method::input_method_unstable_v2::zwp_input_method_v2::Event as InputMethodEvent;
 use zwp_input_method::input_method_unstable_v2::zwp_input_method_v2::ZwpInputMethodV2;
@@ -39,7 +42,7 @@ pub trait HintPurpose {
 #[derive(Clone, Debug)]
 struct IMProtocolState {
     surrounding_text: String,
-    surrounding_cursor: u32,
+    cursor: u32,
     content_purpose: ContentPurpose,
     content_hint: ContentHint,
     text_change_cause: ChangeCause,
@@ -50,7 +53,7 @@ impl Default for IMProtocolState {
     fn default() -> IMProtocolState {
         IMProtocolState {
             surrounding_text: String::new(),
-            surrounding_cursor: 0,
+            cursor: 0,
             content_hint: ContentHint::None,
             content_purpose: ContentPurpose::Normal,
             text_change_cause: ChangeCause::InputMethod,
@@ -64,8 +67,7 @@ struct IMServiceArc<T: 'static + KeyboardVisibility + HintPurpose> {
     im: Main<ZwpInputMethodV2>,
     connector: T,
     pending: IMProtocolState,
-    current: IMProtocolState, // turn current into an idiomatic representation?
-    preedit_string: String,
+    current: IMProtocolState,
     serial: Wrapping<u32>,
 }
 
@@ -81,12 +83,12 @@ impl<T: 'static + KeyboardVisibility + HintPurpose> IMServiceArc<T> {
             connector,
             pending: IMProtocolState::default(),
             current: IMProtocolState::default(),
-            preedit_string: String::new(),
             serial: Wrapping(0u32),
         };
         let im_service = Arc::new(Mutex::new(im_service));
         let im_service_ref = Arc::clone(&im_service);
         im_service.lock().unwrap().assign_filter(im_service_ref);
+        info!("New IMService was created");
         im_service
     }
 
@@ -116,11 +118,18 @@ impl<T: 'static + KeyboardVisibility + HintPurpose> IMServiceArc<T> {
             },
         });
         self.im.assign(filter);
+        info!("The filter was assigned to Main<ZwpInputMethodV2>");
     }
 
-    fn commit_string(&self, text: String) -> Result<(), SubmitError> {
+    fn commit_string(&mut self, text: String) -> Result<(), SubmitError> {
+        info!("Commit string '{}'", text);
         match self.current.active {
             true => {
+                let cursor_position = self.pending.cursor.try_into().unwrap(); // Converts u32 of cursor to usize
+                self.pending
+                    .surrounding_text
+                    .insert_str(cursor_position, &text);
+                self.pending.cursor += text.len() as u32;
                 self.im.commit_string(text);
                 Ok(())
             }
@@ -128,9 +137,18 @@ impl<T: 'static + KeyboardVisibility + HintPurpose> IMServiceArc<T> {
         }
     }
 
-    fn delete_surrounding_text(&self, before: u32, after: u32) -> Result<(), SubmitError> {
+    fn delete_surrounding_text(&mut self, before: u32, after: u32) -> Result<(), SubmitError> {
+        info!(
+            "Delete the surrounding text ({} bytes before and {} after the cursor at {})",
+            before, after, self.pending.active
+        );
         match self.current.active {
             true => {
+                let cursor_position: usize = self.pending.cursor.try_into().unwrap(); // Converts u32 of cursor to usize
+                self.pending.surrounding_text.replace_range(
+                    cursor_position - (before as usize)..cursor_position + (after as usize),
+                    "",
+                );
                 self.im.delete_surrounding_text(before, after);
                 Ok(())
             }
@@ -139,10 +157,12 @@ impl<T: 'static + KeyboardVisibility + HintPurpose> IMServiceArc<T> {
     }
 
     fn commit(&mut self) -> Result<(), SubmitError> {
+        info!("Commit the changes");
         match self.current.active {
             true => {
                 self.im.commit(self.serial.0);
                 self.serial += Wrapping(1u32);
+                self.pending_becomes_current();
                 Ok(())
             }
             false => Err(SubmitError::NotActive),
@@ -154,11 +174,12 @@ impl<T: 'static + KeyboardVisibility + HintPurpose> IMServiceArc<T> {
     }
 
     fn get_surrounding_text(&self) -> String {
+        info!("Requested surrounding text");
         self.current.surrounding_text.clone()
     }
 
     fn handle_activate(&mut self) {
-        self.preedit_string = String::new();
+        info!("handle_activate() was called");
         self.pending = IMProtocolState {
             active: true,
             ..IMProtocolState::default()
@@ -166,30 +187,48 @@ impl<T: 'static + KeyboardVisibility + HintPurpose> IMServiceArc<T> {
     }
 
     fn handle_deactivate(&mut self) {
+        info!("handle_deactivate() was called");
         self.pending.active = false;
     }
+
     fn handle_surrounding_text(&mut self, text: String, cursor: u32, anchor: u32) {
+        info!("handle_surrounding_text() was called");
         self.pending.surrounding_text = text;
-        self.pending.surrounding_cursor = cursor;
+        self.pending.cursor = cursor;
     }
 
     fn handle_text_change_cause(&mut self, cause: ChangeCause) {
+        info!("handle_text_change_cause() was called");
         self.pending.text_change_cause = cause;
     }
 
     fn handle_content_type(&mut self, hint: ContentHint, purpose: ContentPurpose) {
+        info!("handle_content_type() was called");
         self.pending.content_hint = hint;
         self.pending.content_purpose = purpose;
     }
 
     fn handle_done(&mut self) {
+        info!("handle_done() was called");
+        self.pending_becomes_current();
+    }
+
+    fn handle_unavailable(&mut self) {
+        info!("handle_unavailable() was called");
+        self.im.destroy();
+        self.current.active = false;
+        self.connector.hide_keyboard();
+    }
+
+    fn pending_becomes_current(&mut self) {
+        info!("The pending protocol state became the current state");
         let active_changed = self.current.active ^ self.pending.active;
 
         self.current = self.pending.clone();
-        self.pending = IMProtocolState {
-            active: self.current.active,
-            ..IMProtocolState::default()
-        };
+        //self.pending = IMProtocolState {
+        //    active: self.current.active,
+        //    ..IMProtocolState::default()
+        //};
 
         if active_changed {
             if self.current.active {
@@ -200,12 +239,6 @@ impl<T: 'static + KeyboardVisibility + HintPurpose> IMServiceArc<T> {
                 self.connector.hide_keyboard();
             };
         }
-    }
-
-    fn handle_unavailable(&mut self) {
-        self.im.destroy();
-        self.current.active = false;
-        self.connector.hide_keyboard();
     }
 }
 
@@ -229,6 +262,7 @@ impl<T: 'static + KeyboardVisibility + HintPurpose> IMService<T> {
     }
 
     pub fn delete_surrounding_text(&self, before: u32, after: u32) -> Result<(), SubmitError> {
+        info!("Delete surrounding text ");
         self.im_service_arc
             .lock()
             .unwrap()
@@ -242,6 +276,7 @@ impl<T: 'static + KeyboardVisibility + HintPurpose> IMService<T> {
     pub fn is_active(&self) -> bool {
         self.im_service_arc.lock().unwrap().is_active()
     }
+
     pub fn get_surrounding_text(&self) -> String {
         self.im_service_arc.lock().unwrap().get_surrounding_text()
     }
